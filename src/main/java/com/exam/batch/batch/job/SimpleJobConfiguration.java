57ef5d4f64f4e20282995ecdac54ce8dc21b043a
swsong <ssw890720@gmail.com>
2022-04-21 23:54:33 +0900

    1.README 추가 2.스프링 배치 메타데이터 테스트

--- src/main/java/com/exam/batch/batch/job/SimpleJobConfiguration.java
+++ src/main/java/com/exam/batch/batch/job/SimpleJobConfiguration.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,21 +27,35 @@ public class SimpleJobConfiguration {
      *   - Reader, Processor, Writer   
      */
     @Bean
-    public Job simpleJob() {
-        return jobBuilderFactory.get("simpleJob") // simpleJob이름의 Batch Job 등록
-                .start(simpleStep1()) // Job이 simpleStep1 Batch Step을 실행
+    public Job simpleJob1() {
+        return jobBuilderFactory.get("simpleJob1") // simpleJob이름의 Batch Job 등록
+                .start(simpleStep1(null)) // Job이 simpleStep1 Batch Step을 실행
+                .next(simpleStep2(null))
                 .build();
     }
 
-    /**
-     * TODO step을 bean으로 안만들면?
-     */
     @Bean
-    public Step simpleStep1() {
-        return stepBuilderFactory.get("simpleStep1") // simpleStep1이름의 Batch Step 등록
+    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    //throw new IllegalArgumentException("step1 강제예외 발생");
+                    log.info(">>>>> simpleStep2 시작");
+                    log.info(">>>>> 요청일시 : {}", requestDate);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+
+    @Bean
+    @JobScope
+    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep2") // simpleStep1이름의 Batch Step 등록
                 // Step안에서 단일로 수행될 기능 명시
                 .tasklet(((contribution, chunkContext) -> {
-                    log.info(">>>>> simpleStep1 시작");
+                    log.info(">>>>> simpleStep2 시작");
+                    log.info(">>>>> 요청일시 : {}", requestDate);
                     return RepeatStatus.FINISHED;
                 }))
                 .build();
