dc4c90785ea3deaa9ce3cee010cc8a8904a75983
swsong <ssw890720@gmail.com>
2022-04-26 00:03:17 +0900

    1.git test

--- docker/db/domain.sql
+++ docker/db/domain.sql
@@ -6,6 +6,7 @@ create table pay (
     primary key (id)
 ) engine = InnoDB;
 
+
 insert into pay (amount, tx_name, tx_date_time) VALUES (1000, 'trade1', '2018-09-10 00:00:00');
 insert into pay (amount, tx_name, tx_date_time) VALUES (2000, 'trade2', '2018-09-10 00:00:00');
 insert into pay (amount, tx_name, tx_date_time) VALUES (3000, 'trade3', '2018-09-10 00:00:00');
