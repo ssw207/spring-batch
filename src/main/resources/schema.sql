c7006e567e48d66a5ac253b8562e31c4ab48316f
seungwoo.song <seungwoo.song@nhnpayco.com>
2023-01-20 20:24:02 +0900

    refactor: 학습코드 study 브랜치로 분리

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,23 +7,3 @@ create table product
 
     primary key (id)
 );
-
-create table book
-(
-    id bigint not null,
-    primary key (id)
-);
-
-create table book_shop
-(
-    id bigint not null,
-    primary key (id)
-);
-
-create table customer
-(
-    id    varchar(255) not null,
-    name  varchar(255),
-    price integer      not null,
-    primary key (id)
-);
