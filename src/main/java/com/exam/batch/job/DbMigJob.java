56e2f757051cbe6908d8629b4186a1cce54acb66
swsong <ssw890720@gmail.com>
2023-01-23 23:56:03 +0900

    refactor: paging 0번으로 고정

--- src/main/java/com/exam/batch/job/DbMigJob.java
+++ src/main/java/com/exam/batch/job/DbMigJob.java
@@ -8,14 +8,12 @@ import javax.persistence.EntityManagerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
-import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class DbMigJob {
 
+	public static final int CHUNK_SIZE = 1000;
 	private final JobBuilderFactory jobBuilderFactory;
 	private final StepBuilderFactory stepBuilderFactory;
 	private final EntityManagerFactory emf;
@@ -43,27 +42,31 @@ public class DbMigJob {
 	}
 
 	@Bean
-	@JobScope
 	public Step migStep() {
 		return stepBuilderFactory.get("migStep")
-			.<Product, Product2>chunk(1000)
+			.<Product, Product2>chunk(CHUNK_SIZE)
 			.reader(migReader())
 			.writer(migWriter())
 			.build();
 	}
 	@Bean
-	@StepScope
 	public ItemReader<Product> migReader() {
+
 		Map<String, Object> param = new HashMap<>();
 		param.put("id", 0L);
+		JpaPagingItemReader<Product> reader = new JpaPagingItemReader<>() {
+			@Override
+			public int getPage() { // 페이징 사이즈를 0으로 고정한다
+				return 0;
+			}
+		};
 
-		return new JpaPagingItemReaderBuilder<Product>()
-			.entityManagerFactory(emf)
-			.pageSize(1000)
-			.name("migReader")
-			.queryString("SELECT p FROM Product p WHERE p.id > :id")
-			.parameterValues(param)
-			.build();
+		reader.setQueryString("SELECT p FROM Product p WHERE p.id > :id ORDER BY p.id");
+		reader.setPageSize(CHUNK_SIZE);
+		reader.setName("migReader");
+		reader.setParameterValues(param);
+		reader.setEntityManagerFactory(emf);
+		return reader;
 	}
 
 	@Bean
